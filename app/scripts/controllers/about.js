bc3b4ef78d34be184623d1302bd4d897cf977451
JBigs206 <JeffreyRichardBigler@gmail.com>
2016-11-30 11:34:13 -0800

    Created project build.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name authAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the authAppApp
+ */
+angular.module('authAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name authAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the authAppApp
+ */
+angular.module('authAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
